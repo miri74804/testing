54dab431c3d577936b12fca27ac9e2bdb1f685ae
miryam cohen <miri74804@gmail.com>
2026-06-07 01:55:07 +0300

    Add files via upload
    adding file cpp

---
+++ Officer.cpp
@@ -0,0 +1,15 @@
+#include "Officer.h"
+#include <iostream>
+
+Officer::Officer(string id, string fname, string lname, int count, double mark) :Soldier(id, fname, lname, count), mark(mark) {}
+bool Officer::medal() {
+	return count > 2 && mark >= 92;
+}
+void Officer::print() {
+	cout << soldierType() << endl;
+	Soldier::print();
+	cout << "mark: " << mark << endl;
+}
+double Officer::getMark() {
+	return mark;
+}
